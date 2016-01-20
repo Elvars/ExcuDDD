e4de8dac99165b5f9ba52fe196a1344e40a20b67
Samu <samu.91.7@luukku.com>
2016-01-20 17:42:11 +0200

    first commit

---
+++ excuDDD_bot.pl
@@ -0,0 +1,86 @@
+ #!/usr/bin/perl
+
+use DBI;
+use strict;
+use warnings;
+use utf8;
+use Data::Dumper;
+use Encode;
+
+package MyBot;
+use base qw(Bot::BasicBot);
+
+
+binmode (STDOUT, ":utf8");
+
+
+my $driver   = "SQLite";
+my $database = "excuDDD.db";
+my $dsn = "DBI:$driver:dbname=$database";
+my $userid = "";
+my $password = "";
+my $dbh = DBI->connect($dsn, $userid, $password,
+	{
+		RaiseError=>1,
+		AutoCommit=>1,
+		sqlite_unicode=>1
+	}
+	);
+
+my @kiitos = ("viiten tähten postaus", "kiittikiitoos", "hoisss", "kiitos kontribuutiostasi");
+
+sub said {
+      
+      our ($self, $message) = @_;
+      
+      	if($message->{body}=~/!excuDDD/){
+      		if($message->{body}=~/youtu/ or $message->{body}=~/youtube/ ){
+		
+		my $split = $message->{body};
+		my @values = split(' ', $split);
+
+   
+		my $url = $values[1];
+		my $nick=$message->{who};
+        
+         
+		my $stmt = qq(INSERT INTO LINKKI (WHO, URL)
+			VALUES ("$nick", "$url"));
+            
+		my $rv = $dbh->do($stmt) or die $DBI::errstr;
+        
+		
+		my $kiitos=$kiitos[rand @kiitos];		return "$kiitos $nick";
+         
+		}	
+         
+	}
+	
+	
+	if($message->{body} eq "!select"){
+		
+		my $stmt = qq(SELECT DISTINCT URL FROM LINKKI;);
+		my $sth = $dbh->prepare($stmt);
+		my $rv = $sth->execute() or die $DBI::errstr;
+			
+		if($rv < 0){
+			print $DBI::errstr;
+		}
+						
+		while(my @row = $sth->fetchrow_array()){
+			$self->say(
+				channel =>"#yourchannelhere",
+				body=>"$row[0] \n",
+			);
+			
+			
+		}
+		return undef;
+	}
+
+}
+MyBot->new(
+	server => 'yourserverhere',
+	channels => [ '#yourchannelhere'],
+	nick => 'yournickhere',
+)->run();
---
+++ excuDDD_table.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use DBI;
+use strict;
+use utf8;
+
+my $driver   = "SQLite";
+my $database = "excuDDD.db";
+my $dsn = "DBI:$driver:dbname=$database";
+my $userid = "";
+my $password = "";
+my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
+                      or die $DBI::errstr;
+print "Opened database successfully\n";
+
+
+my $stmt = qq(CREATE TABLE LINKKI
+      (
+       ID INTEGER PRIMARY KEY AUTOINCREMENT,
+       WHO TEXT		NOT NULL,
+       URL TEXT		NOT NULL
+       
+       ););
+       
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+$dbh->disconnect();
